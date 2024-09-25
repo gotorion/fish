25cd5b5eba5fa3c852532080a11f085cd6b0c8a9
Junhui Li <junhui.lee@icloud.com>
2024-09-25 11:47:53 +0800

    update: add more alias

--- config.fish
+++ config.fish
@@ -2,3 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 alias ll="eza -l"
+alias gb="git branch | sort"
+alias gc="git commit"
+alias ga="git add"
+alias gw="git switch"
